803f06bf0388a99e4858589153d5926d52a04b80
Anthony McKenna <amckenna@realgreen.com>
2018-04-09 11:40:53 -0400

    Initial commit for stackoverflow question https://stackoverflow.com/questions/49700834/using-powershell-reflection-to-perform-database-updates/49709289

---
+++ PSTest/Areas/Database/Controllers/UpdateController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PSTest.Areas.Database.Controllers
+{
+    public class UpdateController : Controller
+    {
+        // GET: Database/Update
+        public ActionResult Index()
+        {
+            return View(Execute());
+        }
+
+        public static List<string> Execute()
+        {
+            var returnable = new List<string>();
+            
+            var assembly = Assembly.GetExecutingAssembly();
+
+            string codeBase = assembly.CodeBase;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            var assemblyLocation = Path.GetDirectoryName(path);
+            
+            string resourcePath = "";
+
+            string ModuleName = assembly.ManifestModule.Name;
+            ModuleName = ModuleName.Substring(0, ModuleName.LastIndexOf("."));
+            ModuleName = ModuleName.Replace(' ', '_').Replace(".", "");
+
+            string FolderPath = "Areas.Database.SQL";
+            FolderPath = FolderPath.Replace(' ', '_');
+
+            if (FolderPath != null && FolderPath.Length > 0 && FolderPath[FolderPath.Length - 1] == '.')
+                FolderPath = FolderPath.Substring(0, FolderPath.Length - 1);
+
+            StringBuilder filepath = new StringBuilder();
+            filepath.Append(ModuleName);
+            if (FolderPath != null && FolderPath.Length > 0)
+            {
+                filepath.Append('.' + FolderPath);
+                filepath.Append('.');
+            }
+            resourcePath = filepath.ToString();
+
+            string[] resourceNames = assembly.GetManifestResourceNames();
+            foreach (var resourceName in resourceNames)
+            {
+                if (Regex.Match(resourceName, "^" + resourcePath).Success)
+                {
+                    returnable.Add(resourceName);
+                }
+            }
+
+            var orderedFileNames = new List<string>();
+            if (returnable != null && returnable.Any())
+            {
+                orderedFileNames = returnable.OrderBy(q => q).ToList();
+            }
+            else
+            {
+                returnable.Add("No files found");
+            }
+
+            return returnable;
+        }
+    }
+}
---
+++ PSTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PSTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PSTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ebfe9893-b3d6-4be3-b5ed-a0c30d74b8bd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
